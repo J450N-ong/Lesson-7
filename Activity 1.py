d271f7a0cf1819405b0fc28ea4cabf35429fb504
J450N-ong <odeleyejason@gmail.com>
2024-09-28 14:56:41 +0100

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,8 @@
+#Membership Operators
+a="Nabeela"
+b="bee"
+if b in a:
+    print("True")
+c="Ma"
+if c not in a:
+    print("True")
---
+++ Activity 2.py
@@ -0,0 +1,9 @@
+#Identity Operators
+a="Rafael"
+b="Rafael"
+if a is b:
+    print("True")
+c="Daniel"
+d="Daniella"
+if c is not d:
+    print("True")
